bde76233c805a09b55c79fb3ba69976b6dc6c3f5
jmcarth4@gmail.com <jmcarth4@gmail.com>
2023-10-05 17:48:03 -0600

    Add project files.

---
+++ Class_Terminal_2/Form1.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.BaudRateButton = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.InTermListBox = New System.Windows.Forms.ListBox()
+        Me.OutTermListBox = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SendButton = New System.Windows.Forms.Button()
+        Me.PortDataListBox = New System.Windows.Forms.ListBox()
+        Me.PortOpenButton = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.ComPortButton = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(-150, 173)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox1.TabIndex = 37
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(-194, -94)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(165, 204)
+        Me.ListBox1.TabIndex = 35
+        '
+        'BaudRateButton
+        '
+        Me.BaudRateButton.Location = New System.Drawing.Point(-150, 225)
+        Me.BaudRateButton.Name = "BaudRateButton"
+        Me.BaudRateButton.Size = New System.Drawing.Size(127, 95)
+        Me.BaudRateButton.TabIndex = 32
+        Me.BaudRateButton.Text = "Baud Rate"
+        Me.BaudRateButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'InTermListBox
+        '
+        Me.InTermListBox.FormattingEnabled = True
+        Me.InTermListBox.ItemHeight = 20
+        Me.InTermListBox.Location = New System.Drawing.Point(887, 12)
+        Me.InTermListBox.Name = "InTermListBox"
+        Me.InTermListBox.Size = New System.Drawing.Size(325, 244)
+        Me.InTermListBox.TabIndex = 53
+        '
+        'OutTermListBox
+        '
+        Me.OutTermListBox.FormattingEnabled = True
+        Me.OutTermListBox.ItemHeight = 20
+        Me.OutTermListBox.Location = New System.Drawing.Point(578, 12)
+        Me.OutTermListBox.Name = "OutTermListBox"
+        Me.OutTermListBox.Size = New System.Drawing.Size(216, 224)
+        Me.OutTermListBox.TabIndex = 52
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(683, 535)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(154, 104)
+        Me.Button3.TabIndex = 51
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(480, 525)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(154, 104)
+        Me.Button2.TabIndex = 50
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(287, 525)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(154, 104)
+        Me.Button1.TabIndex = 49
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1076, 411)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 48
+        '
+        'SendButton
+        '
+        Me.SendButton.Location = New System.Drawing.Point(936, 283)
+        Me.SendButton.Name = "SendButton"
+        Me.SendButton.Size = New System.Drawing.Size(154, 104)
+        Me.SendButton.TabIndex = 47
+        Me.SendButton.Text = "Send Packet"
+        Me.SendButton.UseVisualStyleBackColor = True
+        '
+        'PortDataListBox
+        '
+        Me.PortDataListBox.FormattingEnabled = True
+        Me.PortDataListBox.ItemHeight = 20
+        Me.PortDataListBox.Location = New System.Drawing.Point(320, 12)
+        Me.PortDataListBox.Name = "PortDataListBox"
+        Me.PortDataListBox.Size = New System.Drawing.Size(173, 204)
+        Me.PortDataListBox.TabIndex = 46
+        '
+        'PortOpenButton
+        '
+        Me.PortOpenButton.Location = New System.Drawing.Point(669, 283)
+        Me.PortOpenButton.Name = "PortOpenButton"
+        Me.PortOpenButton.Size = New System.Drawing.Size(154, 104)
+        Me.PortOpenButton.TabIndex = 45
+        Me.PortOpenButton.Text = "Connect"
+        Me.PortOpenButton.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(320, 279)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox2.TabIndex = 44
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(67, 279)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox3.TabIndex = 43
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 20
+        Me.ListBox2.Location = New System.Drawing.Point(194, 12)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(110, 184)
+        Me.ListBox2.TabIndex = 42
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 20
+        Me.ListBox3.Location = New System.Drawing.Point(23, 12)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(165, 204)
+        Me.ListBox3.TabIndex = 41
+        '
+        'QuitButton
+        '
+        Me.QuitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.QuitButton.Location = New System.Drawing.Point(1076, 546)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(136, 104)
+        Me.QuitButton.TabIndex = 40
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'ComPortButton
+        '
+        Me.ComPortButton.Location = New System.Drawing.Point(287, 333)
+        Me.ComPortButton.Name = "ComPortButton"
+        Me.ComPortButton.Size = New System.Drawing.Size(154, 104)
+        Me.ComPortButton.TabIndex = 39
+        Me.ComPortButton.Text = "Com Port"
+        Me.ComPortButton.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(67, 331)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(127, 95)
+        Me.Button4.TabIndex = 38
+        Me.Button4.Text = "Baud Rate"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1239, 682)
+        Me.Controls.Add(Me.InTermListBox)
+        Me.Controls.Add(Me.OutTermListBox)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.SendButton)
+        Me.Controls.Add(Me.PortDataListBox)
+        Me.Controls.Add(Me.PortOpenButton)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.ComPortButton)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.BaudRateButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents BaudRateButton As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents InTermListBox As ListBox
+    Friend WithEvents OutTermListBox As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SendButton As Button
+    Friend WithEvents PortDataListBox As ListBox
+    Friend WithEvents PortOpenButton As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents ComPortButton As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Class_Terminal_2/Form1.vb
@@ -0,0 +1,199 @@
+﻿
+
+
+
+
+
+'Option Strict On
+'Option Explicit On
+Public Class Form1
+        Dim portState As Boolean
+        Public receiveByte(18) As Byte
+
+
+        Private Sub Form1_UnLoad()
+            Try
+                SerialPort1.Close()
+
+            Catch ex As Exception
+
+            End Try
+        End Sub
+
+
+        Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+            ListBox2.Items.Clear()
+            ComboBox2.Items.Clear()
+            ' portselect.items.clear()
+
+
+
+            portState = False
+
+            SerialPort1.BaudRate = 9600                    '9600 baud rate
+            SerialPort1.DataBits = 8                       '8 data bits
+            SerialPort1.StopBits = IO.Ports.StopBits.One   '1 stop bit
+            SerialPort1.Parity = IO.Ports.Parity.None      'no Parity
+
+            Timer1.Enabled = True                   'timer set to 50 ms
+
+        End Sub
+
+
+    Private Sub ComPortButton_Click(sender As Object, e As EventArgs)
+
+        ComboBox2.Items.Clear()
+        ListBox2.Items.Clear()
+
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ListBox2.Items.Add(sp)
+            ComboBox2.Items.Add(sp)
+        Next
+
+    End Sub
+
+    Private Sub PortOpenButton_Click(sender As Object, e As EventArgs)
+        If PortOpenButton.Text = "Connect" Then
+            Try
+                SerialPort1.Open()
+                PortOpenButton.Text = "Disconnect"
+                portState = True
+            Catch ex As Exception
+                MsgBox("Port Already Open or Port Unavailable")
+                PortOpenButton.Text = "Connect"
+                portState = False
+            End Try
+        Else
+            Try
+                SerialPort1.Close()
+            Catch ex As Exception
+
+            End Try
+            portState = False
+            PortOpenButton.Text = "Connect"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim dataIn As String
+        dataIn = ""
+        PortDataListBox.Items.Clear()
+        PortDataListBox.Items.Add("Com port = " & SerialPort1.PortName) 'show current baud rate
+        PortDataListBox.Items.Add("Baud Rate = " & SerialPort1.BaudRate) 'show current baud rate
+        PortDataListBox.Items.Add("Data bits = " & SerialPort1.DataBits)
+        PortDataListBox.Items.Add("Stop bits = " & SerialPort1.StopBits)
+        PortDataListBox.Items.Add("Parity = " & SerialPort1.Parity)
+
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs)
+        Try
+            SerialPort1.Close()
+
+        Catch ex As Exception
+
+        End Try
+
+        PortOpenButton.Text = "Connect"
+        portState = False
+        Try
+            SerialPort1.BaudRate = ListBox2.SelectedItem 'see if baud rate data is in the list box
+        Catch ex As Exception
+            SerialPort1.PortName = ListBox2.SelectedItem 'bot baud rate, save port name
+        End Try
+    End Sub
+
+
+    Private Sub BaudRateButton_Click(sender As Object, e As EventArgs) Handles BaudRateButton.Click
+        ListBox1.Items.Clear()                          'clear list box and load baud rate values
+        ListBox1.Items.Add(1200)
+        ListBox1.Items.Add(2400)
+        ListBox1.Items.Add(4800)
+        ListBox1.Items.Add(9600)
+        ListBox1.Items.Add(19200)
+        ListBox1.Items.Add(38400)
+        ListBox1.Items.Add(57600)
+        ListBox1.Items.Add(115200)
+        ListBox1.Items.Add(230400)
+        ListBox1.Items.Add(230400)
+        ListBox1.Items.Add(460800)
+        ListBox1.Items.Add(921600)
+
+    End Sub
+
+    Private Sub SendButton_Click(sender As Object, e As EventArgs)
+        Dim dataOut As String
+
+        dataOut = TextBox1.Text
+        If portState = True Then
+            If portState = True Then
+                SerialPort1.Write(dataOut, 0, 1)
+                ListBox1.Items.Add(dataOut)
+            Else
+                MsgBox("Please configure and open serial port to procede")
+
+            End If
+        End If
+
+        Timer1.Enabled = False
+
+        'Dim dataOut1 As String
+        'Dim dataOut2 As String
+        'Dim dataOut3 As String
+        'Dim dataOut4 As String
+        Dim dataIn1, dataIn2, dataIn3, dataIn4 As Integer
+
+
+
+        If portState = True Then
+            If dataOut IsNot "" Then
+                SerialPort1.Write(dataOut, 0, 1)
+                OutTermListBox.Items.Add(dataOut)
+
+            Else
+                Timer1.Enabled = True
+                Exit Sub
+
+            End If
+
+
+            Try
+                SerialPort1.Read(receiveByte, 0, 10)
+                dataIn1 = receiveByte(0)
+                dataIn2 = receiveByte(1)
+                dataIn3 = receiveByte(2)
+                dataIn4 = receiveByte(3)
+                InTermListBox.Items.Add(Chr(dataIn1) & vbTab & Chr(dataIn2) & vbTab & Chr(dataIn3) & vbTab & Chr(dataIn4))
+            Catch ex As Exception
+
+            End Try
+
+        Else
+            MsgBox("Please configure and open serial port to procede")
+            TextBox1.Text = " "
+        End If
+        Timer1.Enabled = True
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        TextBox1.Text = " "
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        OutTermListBox.Items.Clear()
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        InTermListBox.Items.Clear()
+    End Sub
+
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs)
+        Me.Close()
+    End Sub
+
+End Class
+
+
+
